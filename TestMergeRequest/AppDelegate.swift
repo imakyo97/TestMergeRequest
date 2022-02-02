a93c395c68d9afa6ebbd3cfb71ccc4c32023891f
kyohei_i <kyoheishogun.1997@i.softbank.jp>
2022-02-02 22:43:21 +0900

    test8を実装

--- TestMergeRequest/AppDelegate.swift
+++ TestMergeRequest/AppDelegate.swift
@@ -15,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         print("test2を実装")
         print("test2を実装二回目")
+        print("test8を実装")
         return true
     }
 
